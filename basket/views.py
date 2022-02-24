45de5a34a3b62f40094db98e6f7c40cc5d430126
Petrov D <kam1k@mail.ru>
2022-02-24 16:56:59 +0300

    added AJAX basket edit logic

--- basket/views.py
+++ basket/views.py
@@ -3,6 +3,9 @@ from products.models import Product
 from basket.models import Basket
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+
 
 # Create your views here.
 @login_required
@@ -29,3 +32,17 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+@login_required
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        item = Basket.objects.get(id=id)
+        if quantity > 0:
+            item.quantity = quantity
+            item.save()
+        else:
+            item.delete()
+        basket = Basket.objects.filter(user=request.user)
+        context = {'basket': basket}
+        result = render_to_string('basket/basket.html', context)
+        return JsonResponse({'result': result})
