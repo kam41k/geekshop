851517a9774d27f334ff61b19a034342b0d0f5ff
Petrov D <kam1k@mail.ru>
2022-02-28 21:43:12 +0300

    changed django admin views

--- basket/admin.py
+++ basket/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from basket.models import Basket
+
 
 # Register your models here.
+class BasketAdminInline(admin.TabularInline):
+    model = Basket
+    fields = readonly_fields = ('product', 'quantity', 'created_timestamp')
+    extra = 0
--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from products.models import Product, ProductCategory
 
-admin.site.register(Product)
-admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    fields = ('name', 'description')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
